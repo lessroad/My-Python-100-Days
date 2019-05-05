3b52fdbf58a2e83699b1ca578a1ee7eaf2a2c3ec
Youth Yang <8593009@qq.com>
2019-05-05 18:02:37 +0800

    Python 01

--- Day01-15/Day01/work01.py
+++ Day01-15/Day01/work01.py
@@ -1,16 +1,24 @@
 import turtle
+import random
 
 turtle.pensize(14)
 turtle.pensize(3)
 turtle.pencolor('black')
-turtle.forward(200)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(200)
-turtle.right
+for i in range(1,255):
+        #turtle.pencolor(random.randint(1,255),random.randint(1,255),random.randint(1,255))
+        #turtle.color(8, 58, 114)
+        r = random.randint(0, 100)
+        g = random.randint(0, 100)
+        b = random.randint(0, 100)
+        turtle.pencolor(r/100, g/100, b/100)
+        # 向左转3度
+        turtle.circle(i*10)
+        turtle.pu()
+
+        turtle.goto(0,-i*10)
+        turtle.pd()
+        # 向前走
+        #turtle.forward(1)
+
 turtle.mainloop()
 print("测试")
