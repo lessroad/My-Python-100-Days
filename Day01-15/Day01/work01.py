8e49e3b9ec68bb55095ed50bc43ed633db9f44fa
Youth Yang <8593009@qq.com>
2019-05-05 14:53:07 +0800

    测试vscode

---
+++ Day01-15/Day01/work01.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.pensize(14)
+turtle.pencolor('black')
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
