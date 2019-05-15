1c81db07d7f23e90ea1f792d3f3e1ef75a74ef4a
Youth Yang <8593009@qq.com>
2019-05-15 22:55:53 +0800

    Day 01

--- Day01-15/Day01/Hello.py
+++ Day01-15/Day01/Hello.py
@@ -1,12 +1,40 @@
-#import getpass
-import turtle
+"""
+我的第一个Python程序  必须是Hello World
+100天学会Python--Day01
+"""
+#这是一个单行的注释
+"""
+这是一个多行的注释
+"""
 
-username = input('用户名：')
-#getpass模块在pycharm下不可用
-#password = getpass.getpass('密码：')
-password = input('密码：')
-if username=='admin' and password == 'admin':
-	print('正确，通过！')
-else :
-	print ('用户名或密码错误')
+print("Hello World!")
+print("Hello","World", sep=",",end="!")
+print("Hello","World",sep=",",end="!\n")
 
+
+import this
+
+
+"""
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+"""
--- Day01-15/Day01/work01.py
+++ Day01-15/Day01/work01.py
@@ -1,24 +1,16 @@
+"""
+Day01 绘制图形
+"""
+
 import turtle
-import random
 
-turtle.pensize(14)
-turtle.pensize(3)
-turtle.pencolor('black')
-for i in range(1,255):
-        #turtle.pencolor(random.randint(1,255),random.randint(1,255),random.randint(1,255))
-        #turtle.color(8, 58, 114)
-        r = random.randint(0, 100)
-        g = random.randint(0, 100)
-        b = random.randint(0, 100)
-        turtle.pencolor(r/100, g/100, b/100)
-        # 向左转3度
-        turtle.circle(i*10)
-        turtle.pu()
-
-        turtle.goto(0,-i*10)
-        turtle.pd()
-        # 向前走
-        #turtle.forward(1)
-
+turtle.pensize(4)
+turtle.pencolor("red")
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
 turtle.mainloop()
-print("测试")
