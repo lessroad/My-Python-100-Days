3f4c820496f57fa5047a488210dc5bcf8a3e6e7c
Youth Yang <8593009@qq.com>
2019-05-15 22:25:22 +0800

    Day 01

--- Day01-15/Day01/Hello.py
+++ Day01-15/Day01/Hello.py
@@ -1,1 +1,12 @@
-print("hello","world",sep=":::",end="1111\n")
+#import getpass
+import turtle
+
+username = input('用户名：')
+#getpass模块在pycharm下不可用
+#password = getpass.getpass('密码：')
+password = input('密码：')
+if username=='admin' and password == 'admin':
+	print('正确，通过！')
+else :
+	print ('用户名或密码错误')
+
